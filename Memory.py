fbc69bfc2db0accfb61792657f474aadd4000f70
AnMegapro <72275179+AnMegapro@users.noreply.github.com>
2020-12-15 22:36:28 +0300

    Add files via upload

---
+++ Memory.py
@@ -0,0 +1,76 @@
+from tkinter import *
+from random import randint as rnd
+import random
+from tkinter import messagebox
+from math import sqrt
+
+#Здесь можно изменить рамер, нго главное чтоб он был чётным и квадраторм числа 
+count=16
+#Здесь можно изменить высоту блока
+heightOfBlock=150
+symbols=["☎","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢","☀","☁","☂","☃","☻","☕","♔","★","☄","☢"]
+window = Tk()
+
+window.geometry(str(int(sqrt(count)*heightOfBlock))+"x"+str(int(sqrt(count)*heightOfBlock+100)))
+move=0
+firstOpened=-1;
+firstOpenedNum=-1;
+Game=True;
+def Click(event):
+    global move
+    global Game
+    global firstOpened
+    global firstOpenedNum
+    
+    if move%2==0:
+        firstOpened=event.widget.typeP
+        firstOpenedNum=event.widget.num
+        
+        for all in btns:
+            if all.open==False:
+                all["text"]=""
+                
+        moves["text"]="Moves:" +str(int(move/2)+1)
+    btns[event.widget.num]["text"]=symbols[event.widget.typeP]
+    if move%2==1 and firstOpened==event.widget.typeP and firstOpenedNum!=event.widget.num:
+        btns[firstOpenedNum].config(font=("Courier", 50))
+        btns[firstOpenedNum].open=True
+        btns[event.widget.num].config(font=("Courier", 50))
+        btns[event.widget.num].open=True
+        
+    move+=1
+    Game=False
+    for all in btns:
+            if all.open==False:
+                print(str(all.num)+"This")
+                Game=True
+    print(Game)
+    if Game==False:
+        name["text"]="You Won!"
+
+   
+
+window.bind("<Button-1>", Click)
+btns=[]
+pairs=list(range(0,int(count/2)))+list(range(0,int(count/2)))
+random.shuffle(pairs)
+for i in range(count):
+    btn= Button(window, text="");
+    print(pairs[i])
+    btn.typeP= pairs[i]
+    btn.num= i
+    btn.open= False
+    btns.append(btn)   
+    btn.config(font=("Courier", 40))
+    
+for btn in btns:
+    e=sqrt(count)
+    btn.place(x=(btn.num%e)*heightOfBlock, y=(btn.num//e)*heightOfBlock+100, width=heightOfBlock,height=heightOfBlock)
+
+name=Label(window, text="Memory")
+moves=Label(window, text="Moves: 0")
+name.config(font=("Courier", 40))
+moves.config(font=("Courier", 20))
+name.pack()
+moves.place(x=10,y=10)
+window.mainloop()
